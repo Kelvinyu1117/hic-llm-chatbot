0082dd7dc9eeda2f927fc68d2b7b6483a92ad956
Yeung Miu Wan <lowanyeung@gmail.com>
2024-06-01 05:01:18 +0800

    feat: Textarea Messaging with Timestamp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [responseData, setResponseData] = useState(null);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+
+    fetch('http://localhost:5000/api/threads', { method: "post", body: formData })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setResponseData(data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <form method="post" onSubmit={handleSubmit}>
+        <label>Ask Your AI:<br />
+        {responseData && (
+          <div>
+            {responseData.content} | {responseData.timestamp}
+          </div>
+        )}
+        <textarea name="postContent" rows={4} cols={80} />
+        </label>
+        <br />
+        <button type="submit">Send</button>
+    </form>
   )
 }
 
--- server/app.py
+++ server/app.py
@@ -1,8 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from datetime import datetime
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/api/threads', methods=['GET', 'POST'])
+def handle_threads():
+    #Displaying 
+    if request.method == 'POST':
+        content = request.form['postContent']
+        response = {
+            "content": content,
+            "timestamp": datetime.now(),
+        }
+        return jsonify(response), 201
+
+    # http://localhost:5000/api/threads
+    if request.method == 'GET':
+        threads = {
+            "content": "Content 1",
+            "current_time": datetime.now(),
+        }
+        return jsonify(threads)
+
+if __name__ == '__main__':
+    app.run(debug=True)
