de7de3724b126601369ac87b25478f6af48a2c6b
Yeung Miu Wan <69677029+wanyeung@users.noreply.github.com>
2024-06-08 23:06:46 +0800

    feat: Textarea Messaging with Timestamp (#2)
    * feat: Textarea Messaging with Timestamp
    
    * fix: Phrase 1 change based on comments
    
    * Update App.jsx

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dns from 'node:dns'  
 
 // https://vitejs.dev/config/
+dns.setDefaultResultOrder('verbatim')  
 export default defineConfig({
+  server: {  
+    port: 3000, 
+  }, 
   plugins: [react()],
 })
