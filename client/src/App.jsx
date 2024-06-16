a3dc9fba9a8ca7ab816eb8f8d0592838af1765c6
Sukisywang <143873022+Sukisywang@users.noreply.github.com>
2024-06-16 10:36:44 +0800

    turned handleSubmit(e) into async fuunction (#4)
    * turned handleSubmit(e) into async fuunction
    
    * creating server using mirage
    
    * replace last commit for server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,44 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import makeServer from "./server.jsx";
 
 function App() {
   const [responseData, setResponseData] = useState(null);
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
 
     const form = e.target;
     const formData = new FormData(form);
 
     try {
-        const response = await fetch('http://localhost:5000/api/threads', { method: "post", body: formData });
-        const data = await response.json()
-        console.log(data);
-        setResponseData(data);
+      const response = await fetch("/api/threads", {
+        method: "post",
+        body: formData,
+      });
+      const data = await response.json();
+      console.log(data);
+      setResponseData(data);
     } catch (error) {
-        console.log('There was an error', error);
+      console.log("There was an error", error);
     }
   }
   return (
     <form method="post" onSubmit={handleSubmit}>
-        <label>Ask Your AI:<br />
+      <label>
+        Ask Your AI:
+        <br />
         {responseData && (
           <div>
             {responseData.content} | {responseData.timestamp}
           </div>
         )}
         <textarea name="postContent" rows={4} cols={80} />
-        </label>
-        <br />
-        <button type="submit">Send</button>
+      </label>
+      <br />
+      <button type="submit">Send</button>
     </form>
-  )
+  );
 }
 
-export default App
+export default App;
