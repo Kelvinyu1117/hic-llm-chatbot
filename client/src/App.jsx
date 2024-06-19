216d83f9f711a53c76660304092a08b996e6459e
Yeung Miu Wan <lowanyeung@gmail.com>
2024-06-19 08:03:41 +0800

    feat: add front-end radix library & mocking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,34 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState } from 'react'
+import { AccessibilityIcon } from '@radix-ui/react-icons'
+import { Box, Heading, Flex, Text, Button } from '@radix-ui/themes';
 
 function App() {
   const [responseData, setResponseData] = useState(null);
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
 
     const form = e.target;
     const formData = new FormData(form);
 
     try {
-        const response = await fetch('http://localhost:5000/api/threads', { method: "post", body: formData });
+        const response = await fetch('/api/threads', { method: "post", body: formData });
         const data = await response.json()
         console.log(data);
         setResponseData(data);
     } catch (error) {
         console.log('There was an error', error);
     }
+    
   }
   return (
-    <form method="post" onSubmit={handleSubmit}>
-        <label>Ask Your AI:<br />
-        {responseData && (
-          <div>
-            {responseData.content} | {responseData.timestamp}
-          </div>
-        )}
-        <textarea name="postContent" rows={4} cols={80} />
-        </label>
-        <br />
-        <button type="submit">Send</button>
-    </form>
+    <Flex gap="3">
+        <Flex gap="3" py="2" m="4" align = "center">
+          <AccessibilityIcon width="30" height="30"/>
+          <Heading>HIC LLM Chatbot</Heading>
+        </Flex>
+    </Flex>
+    
   )
 }
 
